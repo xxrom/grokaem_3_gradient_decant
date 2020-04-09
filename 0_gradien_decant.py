64091e22115a2a96fc1ea4853d04294ac594c0e1
Chernyshov_Nikita <xpom5566@mail.ru>
2020-04-09 23:09:43 +0300

    feat: gradient decant + alpha correction

--- 0_gradien_decant.py
+++ 0_gradien_decant.py
@@ -1,20 +1,20 @@
 weight = 0.5
 goal_pred = 0.8
 input = 2
-alpha = 1
+alpha = 0.1
 
 # Discrepancy effect (error getting bigger and bigger)
-for interation in range(100):
+for interation in range(20):
 
-  pred = input * weight * alpha
+  pred = input * weight
   error = (pred - goal_pred) ** 2
   delta = pred - goal_pred
 
   # derivative [производная] (how fast changed error by changing weight)
-  weight_delta = delta * input
+  # decrased by alpha coefficient
+  weight_delta = delta * input * alpha
   # Minus because derivative show opposite direction
   weight = weight - weight_delta
 
-  alpha = alpha * 0.9
 
   print('error:  %.04f, prediction: %.04f, delta: %.04f, alpha; %.04f'%(error, pred, delta, alpha))
