2298d3ce702d4c71920580b2e7153e96c8e36965
Chernyshov_Nikita <xpom5566@mail.ru>
2020-04-09 22:03:02 +0300

    feat: too big input and algorithm goes to the wrong way!

--- 0_gradien_decant.py
+++ 0_gradien_decant.py
@@ -1,8 +1,8 @@
-weight = 0.0
+weight = 0.5
 goal_pred = 0.8
-input = 1.1
+input = 2
 
-for interation in range(4):
+for interation in range(20):
   pred = input * weight
   error = (pred - goal_pred) ** 2
   delta = pred - goal_pred
