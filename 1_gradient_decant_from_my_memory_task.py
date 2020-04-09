fed2e1636f5eef1c09e651114ecc5d5e61215cc4
Chernyshov_Nikita <xpom5566@mail.ru>
2020-04-09 23:33:45 +0300

    feat: gradient decant written from my memory (mind) =)

---
+++ 1_gradient_decant_from_my_memory_task.py
@@ -0,0 +1,17 @@
+input = 0.5
+weight = 2
+alpha = 0.9
+pred_goal = 0.234
+
+
+for iteration in range(40):
+  pred = input * weight
+  # cube error (for measurement)
+  cubeError = (pred - pred_goal)**2
+
+  # derivative f(x) =(k*x) => f(x)'= k => k ~~~ input * (pred - pred_goal) ???)
+  weight_delta = input * (pred - pred_goal)
+  # change delta with multiping on alpha coefficient
+  weight -= weight_delta * alpha
+
+  print('cubeError: %.04f, pred: %.04f, weight_delta: %.04f' % (cubeError, pred, weight_delta))
